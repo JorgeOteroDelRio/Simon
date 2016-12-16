fd62dfac7cbe423e4a6ae56864672963248b37b3
Jorge <joterodelrio@danielcastelao.org>
2016-12-16 11:43:08 +0100

    Activity creada con su respectivo layout y activity agregada al manifest

---
+++ main/java/com/example/jota/simonj/MensajePartida.java
@@ -0,0 +1,13 @@
+package com.example.jota.simonj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MensajePartida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensaje_partida);
+    }
+}
